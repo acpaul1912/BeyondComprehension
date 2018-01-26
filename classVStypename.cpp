80e051f6b7753ebea0c9d69bf5919b297e6fb60b
Achintya Paul <34968128+acpaul1912@users.noreply.github.com>
2018-01-27 01:34:43 +0530

    Update classVStypename.cpp

--- classVStypename.cpp
+++ classVStypename.cpp
@@ -3,15 +3,16 @@ class vs typename::
    template<class T> class Widget; // uses “class”
    template<typename T> class Widget; // uses “typename”
 
-   in c++ : There no difference in the above statement  , but there is a difference in advance template programming (it varies from compiler to compiler ).
+   in c++ : There no difference in the above statement  , but there is a difference in advance template programming 
+   (it varies from compiler to compiler ).
 
    Rules to apply the typename : 
 
    1.if the parser encounters a nested dependent name in a template, it assumes that the name is not a type unless you tell it otherwise.
      We do that by putting typename immediately in front of it
 
-   Best Practice: The general rule is simple: anytime you refer to a nested dependent type name in a template, you must immediately precede it by the word
-                    typename.
+   Best Practice: The general rule is simple: anytime you refer to a nested dependent type name in a template,
+   you must immediately precede it by the word  typename.
    
    Example: 
 
@@ -35,8 +36,8 @@ class vs typename::
 
    }
 
-  There is one exception to the above rule , is that typename must not precede nested dependent type names in a list of base classes or as a base class identifier in a member
-  initialization list
+  There is one exception to the above rule , is that typename must not precede nested dependent type names in a list of base classes 
+	  or as a base class identifier in a member initialization list
   
   template<typename T>
   class Derived: public Base<T>::Nested { // base class list: typename not
@@ -50,5 +51,5 @@ class vs typename::
 
   Things to Remember
 	✦ When declaring template parameters, class and typename are interchangeable.
-	✦ Use typename to identify nested dependent type names, except in base class lists or as a base class identifier in a member initialization
-	list.
+	✦ Use typename to identify nested dependent type names, except in base class lists 
+		or as a base class identifier in a member initialization list.
